b3144bc674b53f6e0b8bd9453b199fe67c486e38
tahsin2627 <tahsinhassan280@gmail.com>
2025-06-23 12:28:45 +0600

    Update logger.py

--- netlify/functions/logger.py
+++ netlify/functions/logger.py
@@ -1,1 +1,14 @@
+# File: netlify/functions/logger.py
+
+import json
+
+def handler(event, context):
+    print("--- LOGGER FUNCTION WAS TRIGGERED! THE CONNECTION IS WORKING! ---")
+    
+    print("--- RECEIVED DATA ---")
+    print(json.dumps(event, indent=2))
+    print("--------------------")
+    
+    # Acknowledge the request
+    return {'statusCode': 200, 'body': 'Log recorded.'}
 
